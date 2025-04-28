9fcec59e5023f33e30b8222eb352418a44286b28
Mnrasheed2290 <naim.maria2@gmail.com>
2025-04-28 03:00:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const postsContainer = document.getElementById('posts');
+const loadMoreBtn = document.getElementById('loadMoreBtn');
+
+let postsLoaded = 0;
+const postsPerLoad = 5;
+
+async function fetchPosts() {
+  const res = await fetch('/.netlify/functions/getPosts');
+  const data = await res.json();
+  return data.posts;
+}
+
+function createPostElement(post) {
+  return `
+    <div class="post">
+      <h3><a href="${post.url}" target="_blank">${post.title}</a></h3>
+      <p>${post.excerpt}</p>
+    </div>
+  `;
+}
+
+async function loadPosts() {
+  const posts = await fetchPosts();
+  const newPosts = posts.slice(postsLoaded, postsLoaded + postsPerLoad);
+
+  newPosts.forEach(post => {
+    postsContainer.innerHTML += createPostElement(post);
+    if ((postsLoaded + 1) % 2 === 0) {
+      postsContainer.innerHTML += `<div class="ad-slot">[Ad Placeholder]</div>`;
+    }
+    postsLoaded++;
+  });
+}
+
+loadMoreBtn.addEventListener('click', loadPosts);
+
+window.onload = loadPosts;
